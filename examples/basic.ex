3765685a258b652ec09ba5c1d93794bfe0e2b13a
Josh Adams <josh@isotope11.com>
2014-02-25 15:41:49 -0600

    Tweak basic example to be more raw

--- examples/basic.ex
+++ examples/basic.ex
@@ -4,30 +4,13 @@ defmodule Examples.Basic do
   def start do
     connection = Exdrone.Connection[host: {192,168,1,1}, port: 5556]
     {:ok, drone} = D.start(connection)
-    take_off(drone)
-    forward(drone)
-    spin(drone)
-    land(drone)
-    Process.exit(drone, :kill)
-  end
-
-  defp take_off(drone) do
     drone |> D.take_off
     :timer.sleep(2000)
-  end
-
-  defp land(drone) do
-    drone |> D.land
-    :timer.sleep(2000)
-  end
-
-  defp forward(drone) do
+    drone |> D.hover
+    :timer.sleep(1000)
     drone |> D.forward(0.1)
     :timer.sleep(2000)
-  end
-
-  defp spin(drone) do
-    drone |> D.right(0.1)
-    :timer.sleep(2000)
+    drone |> D.land
+    Process.exit(drone, :kill)
   end
 end
